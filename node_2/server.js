60ae10ff7ef508828be984c429ce2821d9528730
assaduzzaman2177 <assaduzzaman2177@gmail.com>
2021-11-24 09:45:52 +0900

    update

---
+++ node_2/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const port = 3000;
+const host = 'localhost';
+const massage = "Hello World";
+
+////// server start
+
+const app = htttp.createServer(function(request, response) {
+
+})
