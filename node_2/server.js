5c80cf5e696495987c65778b1d6205dfa34f16ea
assaduzzaman2177 <assaduzzaman2177@gmail.com>
2021-11-29 11:39:21 +0900

    gg

--- node_2/server.js
+++ node_2/server.js
@@ -1,11 +1,39 @@
 const http = require('http');
 
-const port = 3000;
-const host = 'localhost';
-const massage = "Hello World";
+require('dotenv').config()
+
+const fs = require('fs');
+const html = fs.readFileSync('index.html');
+
+const querystring = require('querystring');
+
+
+const port = process.env.PORT;
+const host = process.env.HOST;
+const message = "Hello World";
 
 ////// server start
 
-const app = htttp.createServer(function(request, response) {
+const app = http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    //  response.write('<h1>Home Page</h1>');
+    // response.write(message);
+
+    //request
+    let post = ''
+    request.on('data', (value) => {
+        post += value;
+    })
+
+    request.on('end', () => {
+        if (post) {
+            post = querystring.parse(post)
+            console.log(post)
+        }
+    })
+    response.end(html);
 
 })
+
+
+app.listen(port, host);
